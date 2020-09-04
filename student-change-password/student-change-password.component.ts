5331822017d7a5369449099d7083423ca68985e3
Aditya Chilla <adityachilla835@gmail.com>
2020-09-04 23:59:34 +0530

    Update student-change-password.component.ts

--- student-change-password/student-change-password.component.ts
+++ student-change-password/student-change-password.component.ts
@@ -10,20 +10,81 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 })
 
 export class StudentChangePasswordComponent implements OnInit {
-  @Input() password:String
-  @Input() MinimumLength = 8;
-  @Input() MaximumLength = 128;
   @Output() strengthChange = new EventEmitter<number>();
   private colors = ['darkred', 'orangered', 'orange', 'yellowgreen'];
   y:any;
 
+    loginForm: FormGroup;
+
+    error_messages = {
+      'fname': [
+        { type: 'required', message: 'First Name is required.' },
+      ],
+
+      'lname': [
+        { type: 'required', message: 'Last Name is required.' }
+      ],
+
+      'email': [
+        { type: 'required', message: 'Email is required.' },
+        { type: 'minlength', message: 'Email length.' },
+        { type: 'maxlength', message: 'Email length.' },
+        { type: 'required', message: 'please enter a valid email address.' }
+      ],
+
+      'password': [
+        { type: 'required', message: 'password is required.' },
+        { type: 'minlength', message: 'password length.' },
+        { type: 'maxlength', message: 'password length.' }
+      ],
+      'confirmpassword': [
+        { type: 'required', message: 'password is required.' },
+        { type: 'minlength', message: 'password length.' },
+        { type: 'maxlength', message: 'password length.' }
+      ],
+    }
 
 
 
-  constructor(private route:ActivatedRoute)  {
+  constructor(private route:ActivatedRoute,public formBuilder: FormBuilder )  {
+    this.loginForm = this.formBuilder.group({
+        email: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(30)
+        ])),
+        password: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(30)
+        ])),
+        confirmpassword: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(30)
+        ])),
+      }, {
+        validators: this.password.bind(this)
+      });
 
   }
   ngOnInit() {
+  }
+
+  password(formGroup: FormGroup) {
+    const { value: password } = formGroup.get('password');
+    const { value: confirmPassword } = formGroup.get('confirmpassword');
+    return password === confirmPassword ? null : { passwordNotMatch: true };
+  }
+
+  myFunction(){
+    var x = document.getElementById("myInput");
+    if (x.type === "password"){
+       x.type = "text";
+    }
+    else{
+      x.type = "password";
+    }
   }
 
 }
